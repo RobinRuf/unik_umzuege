693f3ab3f0b46db5cea1db90e6e772619ea51809
unknown <robin.ruf@hotmail.com>
2022-08-16 19:11:20 +0200

    Edited Home Component;

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImage from "../assets/heroImage.png";
+import Logo from "../assets/logo.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
       <div>
           <img
-            src={HeroImage}
+            src={Logo}
             alt="Logo"
             className="rounded-2xl mx-auto w-2/3 md:w-full mt-20 md:mt-0 md:ml-4"
           />
@@ -21,10 +21,17 @@ const Home = () => {
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             Unik Umzüge
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="text-gray-500 pt-4 pb-2 max-w-md">
             Deine Profis im Bereich Umzug, Transport, Entsorgung und Räumung!
           </p>
-
+          <p className="text-gray-500 pb-2 max-w-md">
+            Sie wollen einen qualitativ hochwertigen und dennoch preiswerten Umzug? 
+            Kein Problem! Das steht bei uns an der Tagesordnung!
+          </p>
+          <p className="text-gray-500 pb-4 max-w-md">
+            Fordern Sie noch heute Ihre kostenlose und unverbindliche Offerte an 
+            und überzeugen Sie sich selbst!
+          </p>
           <div>
             <Link
               to="portfolio"
@@ -32,7 +39,7 @@ const Home = () => {
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
-              Portfolio
+              Offerte anfordern
               <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
