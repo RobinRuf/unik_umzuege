68a99b67bbad02dcc56e598e81fa526b99747a8a
unknown <robin.ruf@hotmail.com>
2022-08-19 16:16:09 +0200

    Added the Google Ratings link;

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import stars from "../assets/stars.png";
 
 const About = () => {
   return (
@@ -36,10 +37,16 @@ const About = () => {
           • verschiedene Kartonboxen <br />
           • Luftpolsterfolie <br />
           • Seidenpapier <br /><br />
-          Und das Beste: Alles wird von uns vor dem Umzug zu Ihnen nach Hause geliefert! <br />
-          Nach dem Umzug können Sie in Ruhe die Kartons auspacken. Sobald Sie fertig sind 
-          rufen Sie uns an und wir kommen diese bei Ihnen abholen.
+          Und das Alles wird kostenlos zu Ihnen nach Hause geliefert und nach dem erfolgreichen
+          Umzug auch wieder von uns abgeholt!
         </p>
+          <div className="flex flex-col pb-10">
+            <a className="flex flex-col" href="https://www.google.com/search?q=unik+umz%C3%BCge+gmbh&sxsrf=ALiCzsZ7kV7HA1rkMUui08sOueQJ4IlyKw%3A1660753098289&source=hp&ei=yhT9YqXGDuGIur4Ppci7aA&iflsig=AJiK0e8AAAAAYv0i2t-F8iuSlEQDESiKn3xtXlMBGL2V&oq=unik+um&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIECCMQJzILCC4QgAQQxwEQrwEyCAgAEB4QFhAKMgYIABAeEBYyBggAEB4QFjIGCAAQHhAWMgYIABAeEBYyBggAEB4QFjIICAAQHhAWEAo6CwgAEIAEELEDEIMBOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOggIABCABBCxAzoFCC4QgAQ6BQgAEMsBOgUILhDLAVAAWMAHYJ8OaABwAHgAgAG3AYgBnQaSAQM0LjOYAQCgAQE&sclient=gws-wiz#lrd=0x4791b91582d08be7:0xeb0949cccfb27b98,1,,," target="_blank">
+              <img className="w-64 mt-10 mx-auto" src={stars} />
+              <p className="text-lg items-center mx-auto">5 Sterne</p>
+              <p className="text-md items-center mx-auto">30+ Google Bewertungen</p>
+            </a>
+        </div>
       </div>
     </div>
   );
