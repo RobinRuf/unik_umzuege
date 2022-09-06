87903169fd3621619cc29893782066815c7533bf
unknown <robin.ruf@hotmail.com>
2022-09-06 18:34:09 +0200

    Added Description of Services; Styled the services parts;

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 
 const Services = () => {
-  const techs = [
+  const services = [
     {
       id: 1,
       title: "Umzüge",
+      desc: "Jeder Umzug ist individuell! Unsere Umzugshelfer sind gut geschult und haben jahrelange Erfahrung. Ob Marmortisch, Glasvitrine oder Ikea-Schrank - jedes Möbelstück wird von unseren Profis in Rekordzeit demontiert, fachgemäss durch Luftpolsterfolie, Seidenpapier, Schutzdecken und Spanngurte geschützt, transportiert und wieder montiert, während Sie sich entspannen und wichtigerem widmen können!",
       style: "shadow-white",
     },
     {
       id: 2,
       title: "Transporte",
+      desc: "Egal ob National oder International, ob klein oder gross, ob wenig oder viel - wir kennen uns aus! Durch geschickte Koordination mit anderen Aufträgen, können wir die kürzeste Transportzeit und das beste Preis-/Leistungsverhältnis anbieten.",
       style: "shadow-white",
     },
     {
       id: 3,
       title: "Entsorgungen",
+      desc: "Entsorgungen sind immer lästig. Egal ob im Rahmen eines Umzugs oder ob Sie einfach ein altes schweres Möbelstück nicht mehr benötigen. Wir übernehmen dies gerne für! Wir haben einige der grössten Entsorgungsstätten der Schweiz als Partner und können Ihnen dadurch die günstigsten Konditionen für die Entsorgung garantieren.",
       style: "shadow-white",
     },
     {
       id: 4,
       title: "Reinigungen",
+      desc: "Wir pflegen eine gute Partnerschaft zu einem lokalen Reinigungsunternehmen, welches für unsere Kunden die Reinigung der abzugebenden Wohnung übernehmen - mit Abnahmegarantie von Seiten unseres Partners!",
       style: "shadow-white",
     }
   ];
@@ -39,13 +43,13 @@ const Services = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {techs.map(({ id, title, describtion, style }) => (
+          {services.map(({ id, title, desc, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <p className="underline">{title}</p>
-              <p>{describtion}</p>
+              <p className="underline font-bold">{title}</p>
+              <p className="text-sm pt-2 mx-4 text-left">{desc}</p>
             </div>
           ))}
         </div>
