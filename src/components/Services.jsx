017ff57a12eab4ad16c2b6e085644fa9c1345f94
unknown <robin.ruf@hotmail.com>
2022-09-06 18:58:27 +0200

    Bugfixes;

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -31,7 +31,7 @@ const Services = () => {
   return (
     <div
       name="dienstleistungen"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-64 sm:pt-0"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
